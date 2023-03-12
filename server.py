d46899017082550d2b175962fc409c12c483ef56
Christian Turner <keycoderk@gmail.com>
2023-03-11 19:46:50 -0500

    Initial Commit

---
+++ server.py
@@ -0,0 +1,50 @@
+class User:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        # default attributes
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print('----------------')
+        print(self.first_name)
+        print(self.last_name)
+        print(self.email)
+        print(self.age)
+        print(self.is_rewards_member)
+        print(self.gold_card_points)
+
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print('ALREADY ENROLLED')
+        self.is_rewards_member = True
+        self.gold_card_points = 200
+
+    def spend_points(self, amount):
+        if amount > self.gold_card_points:
+            print('NOT ENOUGH POINTS')
+        self.gold_card_points -= amount 
+
+user_christian = User('Christian', 'Turner', 'christ@gmail.com', 21)
+user_christian.enroll()
+user_christian.spend_points(50)
+
+user_catherine = User('Cat', 'Murphy', 'catm@gmail.com', 21)
+user_catherine.enroll()
+user_catherine.spend_points(80)
+
+user_daniel = User('Daniel', 'Vintrov', 'daniel@gmail.com', 21)
+
+user_christian.display_info()
+user_catherine.display_info()
+user_daniel.display_info()
+
+user_christian.enroll()
+
+user_daniel.spend_points(40)
+
+
+
